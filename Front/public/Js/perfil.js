61496a096b15b0e51841c685cee67dbefae4c885
Flavia Arroyo <flavia.e.arroyo@gmail.com>
2022-11-06 10:32:12 -0300

    se mejoro visualizacion de pantallas del front

--- Front/public/Js/perfil.js
+++ Front/public/Js/perfil.js
@@ -1,10 +1,10 @@
-
+/*  valida incorpore imagen
 const wrapper = document.querySelector(".wrapper");
 const fileName = document.querySelector(".file-name");
 const defaultBtn = document.querySelector("#default-btn");
 const customBtn = document.querySelector("#custom-btn");
 const cancelBtn = document.querySelector("#cancel-btn i");
-const img = document.querySelector(".imgjug");
+const img = document.querySelector(".imagen");
 let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
 function defaultBtnActive() {
   defaultBtn.click();
@@ -16,6 +16,7 @@ defaultBtn.addEventListener("change", function () {
     reader.onload = function () {
       const result = reader.result;
       img.src = result;
+      console.log(result)
       wrapper.classList.add("active");
     }
     cancelBtn.addEventListener("click", function () {
@@ -29,3 +30,12 @@ defaultBtn.addEventListener("change", function () {
     fileName.textContent = valueStore;
   }
 });
+
+/* validar que no se envie el formjulario sin que s esten llenos todos los campos */
+let form = document.getElementById("form")
+let boton = document.querySelector(".guardar")
+let nombre = document.getElementById('nombre')
+
+let apellido = document.getElementById("apellido")
+let select = document.getElementById("select")
+   
