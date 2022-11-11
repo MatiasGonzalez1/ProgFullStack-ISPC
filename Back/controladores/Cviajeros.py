c2a34182b2b699536227a30d02249741b757dcc3
Fernando <fer.rosalesok@gmail.com>
2022-11-11 13:54:42 -0300

    Actualización controlador de viajeros

--- Back/controladores/Cviajeros.py
+++ Back/controladores/Cviajeros.py
@@ -1,11 +1,16 @@
+#de la clase comentario
 def asignarComentario(string):
     pass
+
+#de la clase usuario
 def obtenerUsuario():
     pass
 def asignarUsuario(string):
     pass
 def asignarLocalidad(string):
     pass
+
+#de la clase viajeros
 def modificarPublicacion(id_publicacion):
     pass
 def eliminarPublicacion(id_publicacion):
